4c3a582677a839a7efff9bdca5eaf8d7295af0d1
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-01-11 19:54:15 +0800

    modularize some function and extract to files

--- fileHelpers.js
+++ fileHelpers.js
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 
+const ExifImage = require('exif').ExifImage
+
 export function moveFiles(dir, filepaths, newDirname) {
   const dirname = path.dirname(dir + 'test.jpg')
   for (const filepath of filepaths) {
@@ -9,4 +11,42 @@ export function moveFiles(dir, filepaths, newDirname) {
     const to = `${newDirname}/${basename}`
     fs.copyFileSync(from, to)
   }
+}
+
+/**
+ * This function will return all filepaths of jpgs in the input dir
+ * @async
+ * @function
+ * @param {string} dir - target directory.
+ * @returns {Promise<[[string], [string]]>} returns an array of 2, [ userComments, filepaths ].
+ */
+
+export async function getFileComments(dir) {
+  const filepaths = fs
+    .readdirSync(dir)
+    .filter((item) => item.includes('.jpg'))
+    .map((item) => dir + item)
+
+  const userComments = []
+  for (const filepath of filepaths) {
+    const userComment = await getExifUserComment(filepath)
+    userComments.push(userComment.split('<')[0])
+  }
+
+  return [userComments, filepaths]
+}
+
+async function getExifUserComment(path) {
+  const file = fs.readFileSync(path)
+  return new Promise((reslove, reject) => {
+    new ExifImage({ image: file }, (error, exifData) => {
+      if (error) {
+        reject(error)
+      } else {
+        const buffer = exifData.exif.UserComment
+        const userComment = buffer.toString('utf-8')
+        reslove(userComment)
+      }
+    })
+  })
 }
--- main.js
+++ main.js
@@ -1,39 +1,20 @@
-import kmeans from 'node-kmeans'
 import os from 'os'
 import fs from 'fs'
 import path from 'path'
-import { getFileComments } from './getFileComments'
-import { moveFiles } from './fileHelpers'
+import { getFileComments, moveFiles } from './fileHelpers'
+import { getSimilarity } from './cosineSimilarity'
+import { runKmeans } from './K-means'
 
 const dir = os.homedir() + '/Documents/.generation/testing/'
-// const dir = os.homedir() + '/coding/text-similarity/'
-
-// take file paths, and new directory name to put them in
 
 export default async function main() {
-  console.log('Directory: ', dir)
+  // console.log('Directory: ', dir)
   const [data, filepaths] = await getFileComments(dir)
 
-  const similarity = []
-  for (let i = 0; i < data.length; i++) {
-    let row = []
-    for (let k = 0; k < data.length; k++) {
-      if (i === k) continue
-
-      row.push(+calculateSimilarity(data[i], data[k]).toFixed(2))
-    }
-
-    similarity.push(row)
-  }
-
-  // TODO take the top percent and if exceed 20 items or 18, add K and rerun.
-  let runs = 250
-  let K = 3
-  const results = []
-  for (let i = 0; i < runs; i++) {
-    const res = await runKmeans(similarity, K)
-    results.push(res.map((item) => item.clusterInd))
-  }
+  const similarity = getSimilarity(data)
+  const runs = 250
+  const k = 4
+  const results = await runKmeans(similarity, k, runs)
 
   const map = new Map()
   results.forEach((item) => {
@@ -53,10 +34,18 @@ export default async function main() {
       })
   })
 
+  console.log('distinct results: ', map.size)
+
   let groupedIdxes = {}
   let maxOccurance = -Infinity
+  console.log(`groupings has 5% or more occurance: `)
   for (const { idxes, occurance } of map.values()) {
     const percentage = ((occurance / runs) * 100).toFixed(2)
+    if (5 <= percentage)
+      console.log(
+        percentage,
+        idxes.map((item) => item.length).sort((a, b) => a - b)
+      )
     if (occurance < maxOccurance) continue
 
     maxOccurance = Math.max(maxOccurance, occurance)
@@ -66,19 +55,12 @@ export default async function main() {
       idxes,
       filepathGroups: idxes.map((item) => item.map((item) => filepaths[item])),
     }
-
-    // console.log('occur: ', percentage, '% of the times')
-    // console.log(
-    //   'length of the each clusters: ',
-    //   idxes.map((item) => item.length).sort((a, b) => a - b)
-    // )
-    // console.log(
-    //   idxes.sort((a, b) => a[0] - b[0]).map((item) => item.map((item) => item))
-    // )
   }
 
-  // console.log('totalSize: ', map.size)
-  console.log(`groupedIdxes: `, groupedIdxes)
+  console.log(
+    `groupedIdxes: `,
+    groupedIdxes.groupSizes.sort((a, b) => a - b)
+  )
 
   // make new dir
   const newDirnames = groupedIdxes.filepathGroups.map(
@@ -90,67 +72,6 @@ export default async function main() {
   }
 
   for (let i = 0; i < groupedIdxes.filepathGroups.length; i++) {
-    moveFiles(dir, groupedIdxes.filepathGroups[i], newDirnames[i])
+    // moveFiles(dir, groupedIdxes.filepathGroups[i], newDirnames[i])
   }
-  // for (const key of map.keys()) {
-  //   if (map.get(key) >= runs * 0.08) console.log(key, map.get(key))
-  // }
-  // console.log(map.size)
-
-  // const averages = Array.from({ length: results[0].length }, (_, idx) => {
-  //   const sum = results.reduce((acc, curr) => {}, 0)
-  //   return sum / results.length
-  // })
-
-  // Find the best result (lowest average cluster index)
-  // const bestIndex = averages.indexOf(Math.min(...averages))
-  // const bestResult = results.map((res) => res[bestIndex])
-
-  // console.log('Averages:', averages)
-  // console.log('Best Result:', bestResult)
-}
-
-function calculateSimilarity(a, b) {
-  // Tokenize input strings
-  const tokenize = (str) => str.toLowerCase().match(/\w+/g) || []
-
-  // Convert strings to token arrays
-  const tokensA = tokenize(a)
-  const tokensB = tokenize(b)
-
-  // Create a set of unique tokens from both arrays
-  const uniqueTokens = new Set([...tokensA, ...tokensB])
-
-  // Create vectors for both input strings
-  const vectorA = Array.from(uniqueTokens).map((token) =>
-    tokensA.includes(token) ? 1 : 0
-  )
-  const vectorB = Array.from(uniqueTokens).map((token) =>
-    tokensB.includes(token) ? 1 : 0
-  )
-
-  // Calculate dot product
-  const dotProduct = vectorA.reduce((acc, val, i) => acc + val * vectorB[i], 0)
-
-  // Calculate magnitudes
-  const magnitudeA = Math.sqrt(vectorA.reduce((acc, val) => acc + val * val, 0))
-  const magnitudeB = Math.sqrt(vectorB.reduce((acc, val) => acc + val * val, 0))
-
-  // Calculate cosine similarity
-  const similarity = dotProduct / (magnitudeA * magnitudeB)
-
-  return similarity
-}
-
-function runKmeans(similarity, k) {
-  return new Promise((resolve, reject) => {
-    kmeans.clusterize(
-      similarity,
-      { k, maxIterations: 1000, tolerance: 0.0001 },
-      (err, res) => {
-        if (err) reject(err)
-        else resolve(res)
-      }
-    )
-  })
 }
