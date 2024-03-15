231b3d5585b7db0c93003d63d68840e68595e866
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-03-15 21:05:28 +0800

    chore: refactor

--- js/fileHelpers.js
+++ js/fileHelpers.js
@@ -1,15 +1,15 @@
-import fs from 'fs'
-import path from 'path'
+import fs from "fs";
+import path from "path";
 
-const ExifImage = require('exif').ExifImage
+const ExifImage = require("exif").ExifImage;
 
 export function moveFiles(dir, filepaths, newDirname) {
-  const dirname = path.dirname(dir + 'test.jpg')
+  const dirname = path.dirname(dir + "test.jpg");
   for (const filepath of filepaths) {
-    const basename = path.basename(filepath)
-    const from = `${dirname}/${basename}`
-    const to = `${newDirname}/${basename}`
-    fs.renameSync(from, to)
+    const basename = path.basename(filepath);
+    const from = `${dirname}/${basename}`;
+    const to = `${newDirname}/${basename}`;
+    fs.renameSync(from, to);
   }
 }
 
@@ -24,36 +24,35 @@ export function moveFiles(dir, filepaths, newDirname) {
 export async function getFileComments(dir) {
   const filepaths = fs
     .readdirSync(dir)
-    .filter((item) => item.includes('.jpg'))
-    .map((item) => dir + item)
+    .filter((item) => item.includes(".jpg"))
+    .map((item) => dir + item);
 
-  const userComments = []
+  const userComments = [];
   for (const filepath of filepaths) {
-    const userComment = await getExifUserComment(filepath)
+    const userComment = await getExifUserComment(filepath);
     userComments.push(
-      userComment
-        .slice(8)
-        .replace(/[\n_:]/g, ' ')
-        .replace(/[\(\),\d\.<>-]/g, '')
-        .replace(')')
-        .split('Steps')[0]
-    )
+      userComment.slice(8),
+      // .replace(/[\n_:]/g, ' ')
+      // .replace(/[\(\),\d\.<>-]/g, '')
+      // .replace(')')
+      // .split('Steps')[0]
+    );
   }
 
-  return [userComments, filepaths]
+  return [userComments, filepaths];
 }
 
 async function getExifUserComment(path) {
-  const file = fs.readFileSync(path)
+  const file = fs.readFileSync(path);
   return new Promise((reslove, reject) => {
     new ExifImage({ image: file }, (error, exifData) => {
       if (error) {
-        reject(error)
+        reject(error);
       } else {
-        const buffer = exifData.exif.UserComment
-        const userComment = buffer.toString('utf-8')
-        reslove(userComment)
+        const buffer = exifData.exif.UserComment;
+        const userComment = buffer.toString("utf-8");
+        reslove(userComment);
       }
-    })
-  })
+    });
+  });
 }
