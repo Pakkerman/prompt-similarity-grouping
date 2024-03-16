3081cfa7d0fa7d056a31898b20e8170aaa35b82b
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-03-16 19:15:26 +0800

    feat: add verbose option

--- src/opts.ts
+++ src/opts.ts
@@ -1,17 +1,19 @@
 import commandLineArgs from "command-line-args";
+import type { Opts } from "./types";
 
-export type Opts = { targetPath: string; clusters: number; type: string };
 export function getOpts(): Opts {
   const config = [
     { name: "targetPath", alias: "p", type: String },
     { name: "clusters", alias: "c", type: Number, defaultValue: 3 },
     { name: "type", alias: "t", type: String, defaultValue: "jpg" },
+    { name: "verbose", alias: "v", type: Boolean, defaultValue: false },
   ];
   const opts = commandLineArgs(config);
   const parsedOpts: Opts = {
     targetPath: opts.targetPath,
     clusters: opts.clusters,
     type: `.${opts.type}`,
+    verbose: opts.verbose,
   };
   return parsedOpts;
 }
